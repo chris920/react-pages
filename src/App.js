85af051b8221f110976a622c21dc46b22d4a7f10
chris920 <chris920@gmail.com>
2021-07-01 13:29:11 -0700

    scss compiler -> css
    also downgraded node-sass to work with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="container-sm d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+          <div>
+            <nav class="nav justify-content-center float-md-end">
+              <a class="nav-link" href="https://upliftduo.com">Projects</a>
+              <a class="nav-link" href="mailto:chris@dougliu.com">Contact</a>
+            </nav>
+          </div>
+        </header>
+
+        <main class="px-3 text-center">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Christopher Douglas Liu.</h1>
+          <p class="lead m-3">Howdy.</p>
+          <p class="lead m-3">
+            <a href="https://duo.page/chris" class="btn btn-lg btn-secondary border-white bg-white">My Pages</a>
+          </p>
+        </main>
+      </div>
   );
 }
 
