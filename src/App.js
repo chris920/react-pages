f95f1bb6a9c0ec6d3b8bfc8f6840e28705b00150
chris920 <chris920@gmail.com>
2021-12-08 12:20:48 -0500

    title and main

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 
 function App() {
   return (
-      <div class="container-sm d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <header class="mb-auto">
+      <div className="container-sm d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="mb-auto">
           <div>
-            <nav class="nav justify-content-center float-md-end">
-              <a class="nav-link" href="https://upliftduo.com">Projects</a>
-              <a class="nav-link" href="mailto:chris@dougliu.com">Contact</a>
+            <nav className="nav justify-content-center float-md-end">
+              <a className="nav-link" href="https://upliftduo.com">Projects</a>
+              <a className="nav-link" href="mailto:chris@dougliu.com">Contact</a>
             </nav>
           </div>
         </header>
 
-        <main class="px-3 text-center">
+        <main className="px-3 text-center">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Christopher Douglas Liu.</h1>
-          <p class="lead m-3">Howdy.</p>
-          <p class="lead m-3">
-            <a href="https://duo.page/chris" class="btn btn-lg btn-secondary border-white bg-white">My Pages</a>
+          <p className="lead m-3">Howdy.</p>
+          <p className="lead m-3">
+            <a href="https://duo.page/chris" className="btn btn-lg btn-secondary border-white bg-white">My Pages</a>
           </p>
         </main>
       </div>
